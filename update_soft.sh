b5da0d97ce814a8df41bb260bf57b7d6103d3ca3
Fede Diaz <nordri@gmail.com>
2020-08-28 08:48:49 +0200

    Fixed Vagrant version regex

--- update_soft.sh
+++ update_soft.sh
@@ -16,20 +16,20 @@ TMP_DIR=$(mktemp -d)
 
 # Vscodium
 DOWNLOAD_URL=$(curl --silent "https://api.github.com/repos/VSCodium/vscodium/releases/latest" | jq -r '.assets[] | select(.content_type | contains("application/x-debian-package")) | select(.name | contains("amd64")) | .browser_download_url')
-curl -L ${DOWNLOAD_URL} -o $TMP_DIR/vscodium.deb
+curl -L ${DOWNLOAD_URL} --output $TMP_DIR/vscodium.deb
 sudo dpkg -i ${TMP_DIR}/vscodium.deb
 
 # Terraform
 DOWNLOAD_URL=$(curl --silent -L https://www.terraform.io/downloads.html | grep linux_amd64.zip | sed 's:<a href="\(.*\)">.*</a>:\1:' | sed 's:<li>\(.*\).*</li>:\1:' | sed -e 's/^[ \t]*//')
-curl -L ${DOWNLOAD_URL} -o $TMP_DIR/terraform.zip
+curl -L ${DOWNLOAD_URL} --output $TMP_DIR/terraform.zip
 pushd $TMP_DIR
 unzip $TMP_DIR/terraform.zip
 sudo mv terraform /usr/local/bin/terraform
 popd
 
 # Vagrant
-LATEST_VERSION=$(curl --silent https://www.vagrantup.com/ | grep -oh "Download [0-9]\{1\}.[0-9]\{1\}.[0-9]\{1\}" | cut -d" " -f2)
-curl --silent -L https://releases.hashicorp.com/vagrant/${LATEST_VERSION}/vagrant_${LATEST_VERSION}_linux_amd64.zip -o $TMP_DIR/vagrant.zip
+LATEST_VERSION=$(curl --silent https://www.vagrantup.com/ | grep -oh "Download [0-9]\+.[0-9]\+.[0-9]\+" | cut -d" " -f2)
+curl --silent -L https://releases.hashicorp.com/vagrant/${LATEST_VERSION}/vagrant_${LATEST_VERSION}_linux_amd64.zip --output $TMP_DIR/vagrant.zip
 pushd $TMP_DIR
 unzip $TMP_DIR/vagrant.zip
 sudo mv vagrant /usr/local/bin/vagrant
@@ -37,6 +37,6 @@ popd
 
 # Minikube
 pushd $TMP_DIR
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+curl -L --output minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
   && chmod +x minikube \
   && sudo mv minikube /usr/bin/minikube
