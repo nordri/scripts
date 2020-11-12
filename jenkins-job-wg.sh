fe6f729c6310dbd4c116e78298fc8fb015805f31
Fede Diaz <nordri@gmail.com>
2020-11-12 12:08:58 +0100

    jenkins job watchdog

---
+++ jenkins-job-wg.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -e
+
+# Jenkins job watch dog
+# Fede Diaz nordri@gmail.com
+# November 2011
+
+# Jenkins TOKEN is here
+source credentials
+
+[[ "$DEBUG" == "true" ]] && set -x
+
+usage() { echo "Usage: $0 -u JENKINS_USER -j JENKINS_JOB -b JENKINS_BUILD" 1>&2; exit 1; }
+
+# getting arguments
+while getopts ":u:j:b:" option; do
+    case "${option}" in
+        u)
+            JENKINS_USER=${OPTARG}
+            ;;
+        j)
+            JENKINS_JOB=${OPTARG}
+            ;;
+        b)
+            BUILD_NUMBER=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+if [ -z "${JENKINS_USER}" ] || [ -z "${JENKINS_JOB}" ] || [ -z "${BUILD_NUMBER}" ]; then
+    usage
+fi
+
+RES=$(curl -k --silent https://${JENKINS_USER}:${JENKINS_TOKEN}@${JENKINS_URL}/job/Development/job/${JENKINS_JOB}/${BUILD_NUMBER}/api/json | jq -r '.result')
+
+while [ "${RES}" == "null" ];
+do
+    sleep 5m
+    RES=$(curl -k --silent https://${JENKINS_USER}:${JENKINS_TOKEN}@${JENKINS_URL}/job/Development/job/${JENKINS_JOB}/${BUILD_NUMBER}/api/json | jq -r '.result')
+done
